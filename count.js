4df22b83ede56518b32895b4b33dcdf24be865ed
Shareena Begum <shareena.fsp@gmail.com>
2021-07-12 21:35:28 +0200

    console setting

--- count.js
+++ count.js
@@ -6,6 +6,7 @@ let count = 0
 function increment () {
     count += 1
     countEl.textContent= count
+    console.log(`count is => ${count}`)
 }
 
 function save() {
@@ -13,6 +14,38 @@ function save() {
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
-    // console.log(count)
+    console.log(count)
 }
+
+// suppose count= 0
+// increment
+// count= count+1
+// count= 0+1 = 1
+
+// suppose count= 1
+// increment
+// count= count+1
+// count= 1+1 = 2
+
+// suppose count= 2
+// increment
+// count= count+1
+// count= 2+1 = 3
+
+
+// suppose count= 0
+// save
+// countStr= count+ " - "
+// countStr= "0 - "
+
+// suppose count= 1
+// save
+// countStr= count+ " - "
+// countStr= "0 - 1 - "
+
+// suppose count= 2
+// save
+// countStr= count+ " - "
+// countStr= "0 - 1 - 2 - "
+
 
